3b7207ef0a1c808ba7fee7da4c7de266fcb5255b
BZIvanov <ivanov.biser@gmx.com>
2021-09-16 09:57:14 +0300

    docker volumes info update

--- Docker/demo-c/src/App.test.js
+++ Docker/demo-c/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders works react text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = screen.getByText(/works/i);
+  expect(el).toBeInTheDocument();
 });
