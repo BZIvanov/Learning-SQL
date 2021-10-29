71bccee3cdb57b449e111ff7fb31c6268a2e413f
BZIvanov <biser.z.ivanov@gmail.com>
2021-10-29 21:42:48 +0300

    sql exercise using procedure

---
+++ SQL/exercises/full-name/setup.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE user;
+
+USE user;
+
+-- After calling the below procedure you should get the following result:
+-- Iva Ivanova
+
+CALL GetFullName('Iva', 'Ivanova');
---
+++ SQL/exercises/full-name/task.md
@@ -0,0 +1,3 @@
+Using procedure get user full name.
+
+_Use the setup.sql file where you can see how the procedure will be called._
